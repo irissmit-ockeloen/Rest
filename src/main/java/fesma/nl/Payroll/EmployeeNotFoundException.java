330a496b99b7a662ef9de883a56c1a316bdcd8e4
Iris Smit-Ockeloen <iris.smit-ockeloen@fesma.nl>
2023-01-09 21:13:19 +0100

    add rest api

---
+++ src/main/java/fesma/nl/Payroll/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package fesma.nl.Payroll;
+
+class EmployeeNotFoundException extends RuntimeException {
+
+	EmployeeNotFoundException(Long id) {
+		super("Could not find employee " + id);
+	}
+}
---
+++ src/main/java/fesma/nl/Payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package fesma.nl.Payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
