18bb1738fb27f0494042709c0e1592319435d6cc
denizvan <119442994+denizvan@users.noreply.github.com>
2023-01-26 11:24:52 +0100

    Feature/cva 74 unit testing (#4)
    * Add unit test examples
    
    * Revert "Revert "in case we can't find the id for the put we throw an exception instead of created a new one""
    
    This reverts commit a12346d0ae0add95192572c4bcfaea48b4b95f8d.
    
    (cherry picked from commit 387cdfdb22823e13d031ec5921d3b3b121c6b3f6)
    
    * Add ProfileControllerTest with the first test: getProfilesShouldReturnAllProfiles
    
    * ProfileControllerTest completed. We have eight @Test.
    In ProfileController some changes.
    
    * ProfileControllerTest completed.
    Some changes I did;
    1- src/main/java/Profile/SpringBootTest.java removed.
    2- src/test/java/Profile/ProfileControllerTests.java removed.
    3- RECORD_3 deleted
    
    Co-authored-by: Iris Smit-Ockeloen <iris.smit-ockeloen@fesma.nl>

---
+++ src/test/java/Profile/ProfileControllerTest.java
@@ -0,0 +1,89 @@
+package Profile;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class ProfileControllerTest {
+    @MockBean
+    ProfileRepository mockRepository;
+
+    @Autowired
+    ProfileController subject;
+
+    Profile RECORD_1 = new Profile("Teacher ", "English", "England");
+    Profile RECORD_2 = new Profile("Java developer", "Dutch", "America");
+
+    @Test
+    void getProfilesShouldReturnAllProfiles() {
+        List<Profile> expected = List.of(RECORD_1, RECORD_2);
+        when(mockRepository.findAll()).thenReturn(expected);
+
+        List<Profile> actual = subject.getProfiles();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void postProfileShouldSaveNewProfileToRepository() {
+        subject.postProfile(RECORD_1);
+
+        verify(mockRepository, times(1)).save(RECORD_1);
+    }
+
+    @Test
+    void getProfileShouldReturnProfileWhenProfileExist() {
+        when(mockRepository.findById(1L)).thenReturn(Optional.of(RECORD_1));
+
+        Object actual = subject.getProfile(1L);
+
+        assertEquals(RECORD_1, actual);
+    }
+
+    @Test
+    void getProfileShouldThrowWhenProfileDoesNotExist() {
+        when(mockRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ProfileNotFoundException.class, () -> {
+            subject.getProfile(1L);
+        });
+    }
+
+    @Test
+    void putProfileShouldSaveNewProfileWhenProfileExist() {
+        when(mockRepository.findById(1L)).thenReturn(Optional.of(RECORD_1));
+
+        subject.putProfile(RECORD_2, 1L);
+
+        verify(mockRepository, times(1)).save(RECORD_2);
+    }
+
+    @Test
+    void putProfileShouldThrowWhenProfileDoesNotExist() {
+        when(mockRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(ProfileNotFoundException.class, () -> {
+                subject.putProfile(RECORD_2, 1L);
+        });
+    }
+
+    @Test
+    void deleteProfileShouldDeleteProfileFromRepository() {
+        subject.deleteProfile(1L);
+
+        verify(mockRepository, times(1)).deleteById(1L);
+    }
+}
+
+
+
