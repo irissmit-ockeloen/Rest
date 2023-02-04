fa1b71dc3ac9f4e526999dba59632e65d16e75a8
denizvan <119442994+denizvan@users.noreply.github.com>
2023-02-04 07:54:30 +0100

    Feature/cvs 75 integration test (#6)
    * Add integration test example
    
    * Add ProfileControllerIT (wip)
    
    * fix ProfileControllerIT
    
    * We Test all the tests and covered the Profile.
    
    ---------
    
    Co-authored-by: Iris Smit-Ockeloen <iris.smit-ockeloen@fesma.nl>

--- src/test/java/Profile/ProfileApplicationTest.java
+++ src/test/java/Profile/ProfileApplicationTest.java
@@ -1,4 +1,4 @@
-package fesma.nl.Payroll;
+package Profile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class PayrollApplicationTest {
+class ProfileApplicationTest {
 
-	@Autowired
-	PayrollApplication subject;
+    @Autowired
+    ProfileApplication subject;
 
-	@Test
-	void smokeTest() {
-		assertNotNull(subject);
-	}
+    @Test
+    void smokeTest() {
+        assertNotNull(subject);
+    }
 
 }
