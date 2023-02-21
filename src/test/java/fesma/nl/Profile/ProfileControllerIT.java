71e84067b37411908892af6bbfcb820a401dba61
Deniz <deniz.van.iersel@fesma.nl>
2023-02-21 16:10:41 +0100

    did comments

--- src/test/java/fesma/nl/Profile/ProfileControllerIT.java
+++ src/test/java/fesma/nl/Profile/ProfileControllerIT.java
@@ -39,7 +39,7 @@ public class ProfileControllerIT {
         // ACT: GET all profiles
         ResponseEntity<String> response = restTemplate.getForEntity(HOST.get(), String.class);
 
-        // CHECK: check the return code
+        // CHECK: the return code
         assertEquals(200, response.getStatusCode().value());
     }
 
@@ -49,22 +49,23 @@ public class ProfileControllerIT {
         ResponseEntity<String> responseEntity = restTemplate
                 .postForEntity(HOST.get(), RECORD_1, String.class);
 
-        // CHECK: check the return code
+        // CHECK: the return code
         assertEquals(200, responseEntity.getStatusCode().value());
     }
 
     @Test
     public void testPostProfileShouldChangeData() {
-        // PREPARE: Post a profile  and change data with exchange() method
+        // ACT: Post a profile
         Profile actual = restTemplate
                 .postForObject(HOST.get(), RECORD_1, Profile.class);
 
+        // CHECK: if the returned Profile is equl to the posted profile and the EXISTING is set
         assertNotNull(actual.getId());
         assertEquals(RECORD_1.getTitle(), actual.getTitle());
         assertEquals(RECORD_1.getFunction(), actual.getFunction());
         assertEquals(RECORD_1.getDescription(), actual.getDescription());
 
-        // CHECK: Return resource and use get method
+        // CHECK: is the database is updated using the get method
         actual = restTemplate
                 .getForObject(HOST.get() + "/" + actual.getId(), Profile.class);
         assertEquals(RECORD_1.getTitle(), actual.getTitle());
@@ -74,19 +75,21 @@ public class ProfileControllerIT {
 
     @Test
     public void testGetProfileShouldReturn200() {
-        // PREPARE: Add a profile to get a id that we can get
-        String id = postProfile(RECORD_1);
+        // PREPARE: Add a profile to get a EXISTING that we can get
+        String EXISTING = postProfile(RECORD_1);
+        
         // ACT: Get the profile
         ResponseEntity<String> responseEntity = restTemplate
-                .getForEntity(HOST.get() + "/" + id, String.class);
-        // CHECK: check the return code
+                .getForEntity(HOST.get() + "/" + EXISTING, String.class);
+        
+        // CHECK: the return code
         assertEquals(200, responseEntity.getStatusCode().value());
     }
 
 
     @Test
     public void testGetNonExistingProfileShouldReturn404() {
-        // ACT: Get the profile, Add 'NON_EXISTING
+        // ACT: Get the profile for a NON_EXISTING profile
         ResponseEntity<String> responseEntity = restTemplate
                 .getForEntity(HOST.get() + "/" + NON_EXISTING, String.class);
 
@@ -96,22 +99,21 @@ public class ProfileControllerIT {
 
     @Test
     public void testPutProfileShouldReturn200() throws JsonProcessingException {
-        // PREPARE: Add a profile to PUT id that we can put
-        String id = postProfile(RECORD_1);
+        // PREPARE: Add a profile to PUT
+        String EXISTING = postProfile(RECORD_1);
 
         // ACT: Put the profile
         ResponseEntity<String> responseEntity = restTemplate.exchange(
-                HOST.get() + "/" + id,
+                HOST.get() + "/" + EXISTING,
                 PUT, getHttpEntity(RECORD_1), String.class);
 
-        // CHECK: check the return code
+        // CHECK: the return code
         assertEquals(200, responseEntity.getStatusCode().value());
-
     }
 
     @Test
     public void testPutNonExistingProfileShouldReturn404() throws JsonProcessingException {
-        // ACT: For return use "PUT" method it put the profile, add 'NON-EXISTING' if profile non-exist
+        // ACT: Put a NON-EXISTING profile
         ResponseEntity<String> responseEntity = restTemplate.exchange(
                 HOST.get() + "/" + NON_EXISTING,
                 PUT, getHttpEntity(RECORD_1), String.class);
@@ -121,26 +123,25 @@ public class ProfileControllerIT {
     }
 
     @Test
-    public void testPutProfileShouldChangeData() throws JsonProcessingException {
-        // PREPARE: Add a profile to get id that we can put id
-        String id = postProfile(RECORD_1);
+    public void testPutProfileShouldChangeData() throws JsonProcessingException, InterruptedException {
+        // PREPARE: Add a profile to put
+        String EXISTING = postProfile(RECORD_1);
 
-        // ACT: Add an id
-        // and delete "/1" ---> to this "/"
+        // ACT: Add an EXISTING
         Profile actual = restTemplate.exchange(
-                HOST.get() + "/" + id,
+                HOST.get() + "/" + EXISTING,
                 HttpMethod.PUT, getHttpEntity(RECORD_2), Profile.class).getBody();
 
-        // CHECK: assertNotNull Check that an object is not null
-        // assertEquals Check string with object
-        assertNotNull(actual.getId());
+        // CHECK: the returned Profile is equal to the input profile
+        assertEquals(EXISTING, actual.getId());
         assertEquals(RECORD_2.getTitle(), actual.getTitle());
         assertEquals(RECORD_2.getFunction(), actual.getFunction());
         assertEquals(RECORD_2.getDescription(), actual.getDescription());
 
-        // Return resource and use get method
+        // CHECK: the database using get method
+        Thread.sleep(200); // Make sure the db get time to process
         actual = restTemplate
-                .getForObject(HOST.get() + "/" + actual.getId(), Profile.class);
+                .getForObject(HOST.get() + "/" + EXISTING, Profile.class);
         assertEquals(RECORD_2.getTitle(), actual.getTitle());
         assertEquals(RECORD_2.getFunction(), actual.getFunction());
         assertEquals(RECORD_2.getDescription(), actual.getDescription());
@@ -148,12 +149,12 @@ public class ProfileControllerIT {
 
     @Test
     public void testDeleteProfileShouldReturn200() {
-        // PREPARE: Add a profile to get a id that we can delete
-        String id = postProfile(RECORD_1);
+        // PREPARE: Add a profile to get an id that we can delete
+        String EXISTING = postProfile(RECORD_1);
 
         // ACT: Delete the profile
         ResponseEntity<String> responseEntity = restTemplate
-                .exchange(HOST.get() + "/" + id, DELETE, getHttpEntity(), String.class);
+                .exchange(HOST.get() + "/" + EXISTING, DELETE, getHttpEntity(), String.class);
 
         // CHECK: check the return code
         assertEquals(200, responseEntity.getStatusCode().value());
